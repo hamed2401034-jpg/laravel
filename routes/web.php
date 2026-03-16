c4516aeabf2c710ffb6ff43fc9d36531db0c7dfa
hamed2401034-jpg <hamed2401034@gmail.com>
2026-03-16 02:46:16 +0000

    Initial Project Setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Http\Request;
+Route::post('/login', function (Request $request) {
+    return "Welcome " . $request->email . "! System is now monitoring for threats.";
+});
